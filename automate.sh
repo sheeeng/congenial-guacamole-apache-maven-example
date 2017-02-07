ded65f264db3808be98c54f906fe4685cfa35868
Leonard Lee <sheeeng@gmail.com>
2017-02-07 16:15:00 +0100

    Add automated script.

---
+++ automate.sh
@@ -0,0 +1,22 @@
+# Increment version of pom.xml file.
+# http://stackoverflow.com/a/36434565
+# http://www.mojohaus.org/build-helper-maven-plugin/parse-version-mojo.html
+
+rainbow --config=mvn3 -- mvn \
+build-helper:parse-version versions:set -DnewVersion=\
+\${parsedVersion.majorVersion}.\
+\${parsedVersion.minorVersion}.\
+\${parsedVersion.nextIncrementalVersion} \
+versions:commit
+
+rainbow --config=mvn3 -- mvn \
+-DinteractiveMode=false \
+-Dgitenv.remoteurl=$(git config --get remote.origin.url) \
+-Dgitenv.branchname=$(git rev-parse --abbrev-ref HEAD) \
+-Dgitenv.commitsCount=$(git rev-list HEAD | wc -l) \
+-Dgitenv.revision=$(git rev-parse HEAD) \
+-Dgitenv.shortrevision=$(git rev-parse --short HEAD) \
+clean deploy && \
+jar xvf target/original-*.jar META-INF/MANIFEST.MF && \
+cat META-INF/MANIFEST.MF
+
