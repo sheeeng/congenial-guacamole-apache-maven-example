031558cb6f5a6c429923e5c4835a5bc7164f9dac
Leonard Lee <sheeeng@gmail.com>
2017-02-07 14:55:00 +0100

    Add initial project files.

---
+++ src/main/java/guacamole/Potato.java
@@ -0,0 +1,11 @@
+package guacamole;
+
+public class Potato {
+    public String shoutHello() {
+        return "Hello!";
+    }
+
+    public String ropeHei() {
+        return "Hei!";
+    }
+}
---
+++ src/main/java/guacamole/PotatoMain.java
@@ -0,0 +1,9 @@
+package guacamole;
+
+public class PotatoMain {
+    public static void main(String[] args) {
+        Potato potato = new Potato();
+        System.out.println(potato.shoutHello());
+        System.out.println(potato.ropeHei());
+    }
+}
